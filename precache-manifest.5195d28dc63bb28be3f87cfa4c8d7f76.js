1a428b6cf368198eee3a91257ae067d1ac0dfe49
rozsabence <robence@gmail.com>
2019-04-26 20:37:21 +0200

    Updates

---
+++ precache-manifest.5195d28dc63bb28be3f87cfa4c8d7f76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b1c052fc92f8803edd47bcde285d29",
+    "url": "/burger-app/index.html"
+  },
+  {
+    "revision": "f6fa99f19717c80c61c9",
+    "url": "/burger-app/static/css/main.ceee8cfb.chunk.css"
+  },
+  {
+    "revision": "d8de2533713a89c80cec",
+    "url": "/burger-app/static/js/2.bc30355d.chunk.js"
+  },
+  {
+    "revision": "f6fa99f19717c80c61c9",
+    "url": "/burger-app/static/js/main.13749b01.chunk.js"
+  },
+  {
+    "revision": "8ee138cb49c5963e2e75",
+    "url": "/burger-app/static/js/runtime~main.20ac298b.js"
+  }
+]);
